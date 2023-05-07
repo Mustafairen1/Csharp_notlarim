06db74511b9c08eac6fd655cc23af38bc5a2a554
Mustafairen1 <130787762+Mustafairen1@users.noreply.github.com>
2023-05-07 19:08:25 +0300

    Calisma4

---
+++ Calismalarim/!!!! 4 Urun KDV Hesaplayici/Calisma4/Calisma3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Calisma3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.DodgerBlue;
+            label1.Location = new Point(12, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Ürün fiyatı:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.PowderBlue;
+            textBox1.Location = new Point(161, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(54, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.ActiveCaption;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 84);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(496, 349);
+            listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(221, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 50);
+            button1.TabIndex = 5;
+            button1.Text = "Hesapla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Highlight;
+            label2.Location = new Point(12, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 25);
+            label2.TabIndex = 2;
+            label2.Text = "İndirim Oranı:";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.PowderBlue;
+            textBox2.Location = new Point(161, 55);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(54, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(520, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Ürün KDV Hesaplama aracı.";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
+        private Label label2;
+        private TextBox textBox2;
+    }
+}
---
+++ Calismalarim/!!!! 4 Urun KDV Hesaplayici/Calisma4/Calisma3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calisma3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int urunFiyati;
+            int indirimOrani;
+            double kdvDahilFiyat;
+            double kdvHaricFiyat;
+            double kdvDahilindirimliFiyat;
+            urunFiyati = Convert.ToInt16(textBox1.Text);
+            indirimOrani = Convert.ToInt16(textBox2.Text);
+            kdvDahilFiyat = (urunFiyati) * 1.18;
+            kdvHaricFiyat = (urunFiyati);
+            kdvDahilindirimliFiyat = ((urunFiyati / 100) * indirimOrani) * 1.18;
+            listBox1.Items.Add(" KDV Dahil Fiyatı: " + kdvDahilFiyat);
+            listBox1.Items.Add(" KDV Hariç Fiyatı: " + kdvHaricFiyat);
+            listBox1.Items.Add(" KDV Dahil İndirimli Fiyat: " + kdvDahilindirimliFiyat);
+        }
+    }
+}
